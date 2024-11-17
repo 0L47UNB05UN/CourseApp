a082ce1adbf10361209ea2a446e5d43ff4672be6
0L47UNB05UN <ikupolatiolatunbosun@gmail.com>
2024-11-17 04:52:21 +0100

    Course app, makes use of LazyVerticalGrid Composable to display a list of courses available

--- app/src/main/java/com/example/coursesapp/MainActivity.kt
+++ app/src/main/java/com/example/coursesapp/MainActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.coursesapp.ui.theme.CoursesAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,15 +50,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CourseApp(modifier: Modifier = Modifier){
-    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+        ) {
         items(Datasource().loadList()){listItems ->
             TopicCard(
                 listItems.name,
                 listItems.num,
                 listItems.image,
-                modifier.fillMaxWidth().padding(
-                    bottom=8.dp, start=8.dp, end=8.dp
-                )
+                modifier.padding(bottom=8.dp, end=8.dp)
             )
         }
     }
@@ -72,7 +72,7 @@ fun TopicCard(
     @DrawableRes image: Int,
     modifier: Modifier = Modifier
 ){
-    Card(modifier){
+    Card(modifier.fillMaxWidth()){
         Row{
             Image(
                 painter = painterResource(image),
